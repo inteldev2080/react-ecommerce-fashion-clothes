802b610d1b2569e538d04a8e26d6d4c064ceb812
Apple <apple@Apples-MacBook-Air.local>
2022-09-12 21:47:53 +0530

    feature/ footer done

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -55,10 +55,10 @@ export default function Home({
           />
         ))}
       </Grid>
-      <Hero
+      {/* <Hero
         headline=" Dessert dragée halvah croissant."
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      />
+      /> */}
       {/* <HomeAllProductsGrid
         newestProducts={products}
         categories={categories}
