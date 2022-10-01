a341ab32c7df478f05b5647ebf03be299f4700b4
Mateusz Juszczyk <matixovi@gmail.com>
2022-10-01 23:55:22 +0200

    Cleanup

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,5 +1,4 @@
 import commerce from '@lib/api/commerce'
-import { Hero } from '@components/ui'
 import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import HeroNav from '@components/common/HeroNav'
 import { LayoutNoNavbar } from '@components/common/Layout/Layout'
@@ -41,10 +40,6 @@ export default function Home({
   return (
     <>
       <HeroNav products={products} />
-      <Hero
-        headline=" Dessert dragée halvah croissant."
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      />
       <HeroFooter />
     </>
   )
